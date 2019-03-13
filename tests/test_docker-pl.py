d4d0f3b446c275191ef42cc585709d37715cfbcb
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-03-13 02:06:20 -0400

    renamed and updated tests files

--- tests/test_docker-pl.py
+++ tests/test_docker-pl.py
@@ -3,53 +3,53 @@
 print("\n[INFO] Running tests on Docker PL image...\n")
 
 import docker
-import unittest
+import unittest2 as unittest
 
 client = docker.from_env()
 
 class TestLanguages(unittest.TestCase):
     def test_python(self):
         print("[INFO] Checking Python3 installation")
-        response = client.containers.run("pyoey/pl", command="which python3", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which python3", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/python3\n", "Python3 missing!")
     
     def test_cpp(self):
         print("[INFO] Checking gcc and gdb installation")
-        response = client.containers.run("pyoey/pl", command="which gcc", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which gcc", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/gcc\n", "gcc missing!")
-        response = client.containers.run("pyoey/pl", command="which gdb", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which gdb", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/gdb\n", "gdb missing!")
 
     def test_lisp(self):
         print("[INFO] Checking Lisp installation")
-        response = client.containers.run("pyoey/pl", command="which sbcl", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which sbcl", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/sbcl\n", "Lisp (sbcl) missing!")
 
     def test_haskell(self):
         print("[INFO] Checking Haskell installation")
-        response = client.containers.run("pyoey/pl", command="which ghc", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which ghc", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/ghc\n", "Haskell (ghc) missing!")
 
     def test_scheme(self):
         print("[INFO] Checking MIT-Scheme installation")
-        response = client.containers.run("pyoey/pl", command="which scheme", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which scheme", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/scheme\n", "MIT-Scheme missing!")
 
 class TestEditors(unittest.TestCase):
     def test_nano(self):
         print("[INFO] Checking Nano installation")
-        response = client.containers.run("pyoey/pl", command="which nano", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which nano", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/bin/nano\n", "Nano missing!")
 
     def test_vim(self):
         print("[INFO] Checking vim installation")
-        response = client.containers.run("pyoey/pl", command="which vim", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="which vim", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/vim\n", "vim missing!")
 
 class TestSampleCode(unittest.TestCase):
     def test_sample(self):
         print("[INFO] Testing sample C++ code ")
-        response = client.containers.run("pyoey/pl", command="./run_sample.sh", remove=True).decode("utf-8")
+        response = client.containers.run("pl", command="./run_sample.sh", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "Hello, World!\n", "Error in Hello World sample!")
 
 if __name__ == '__main__':
