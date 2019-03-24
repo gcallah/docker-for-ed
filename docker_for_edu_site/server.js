8d773da5ef6a84750913c111474695d582d98f0c
Praphulla Bhawsar <prtsh32@gmail.com>
2019-03-24 19:31:36 -0400

    Setting up backend for modifications to documentation JSON

---
+++ docker_for_edu_site/server.js
@@ -0,0 +1,28 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const auth = require('koa-basic-auth')
+
+const PORT = 8081
+const USERNAME = "docker4ed"
+const PASSWORD = "docker4ed"
+
+const app = new Koa()
+const router = new Router()
+
+const editHandler = async (ctx) => {
+    console.log("REQUEST RECEIVED!")
+    ctx.status = 200
+    ctx.body = {
+        'data': 'Wait for it'
+    }
+}
+
+router.use("/edit", auth({ name: USERNAME, pass: PASSWORD }))
+router.post("/edit", editHandler)
+router.get("/login", (ctx) => ctx.status=200)
+
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.listen(PORT)
+console.log(`🌍  Server listening on port ${PORT}`)
