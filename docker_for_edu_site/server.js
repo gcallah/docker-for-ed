f3665ce34589f624abf53704d29aff64edec6662
PrafulB <prtsh32@gmail.com>
2019-03-31 23:06:08 -0400

    Working /edit endpoint

--- docker_for_edu_site/server.js
+++ docker_for_edu_site/server.js
@@ -1,18 +1,30 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const auth = require('koa-basic-auth')
+const bodyParser = require('koa-bodyparser')
+const fs = require('fs')
 
 const PORT = 8081
 const { USERNAME, PASSWORD } = process.env
 
 const app = new Koa()
 const router = new Router()
+app.use(bodyParser())
 
 const editHandler = async (ctx) => {
+    // TODO: Add Error Handling
     console.log("Edit REQUEST RECEIVED!")
-    ctx.status = 200
-    ctx.body = {
-        'data': 'Wait for it'
+    const componentsLocation = `${__dirname}/src/data/`
+    const componentRequested = `${ctx.request.body.component}.json`
+    
+    const files = fs.readdirSync(componentsLocation)
+    
+    if(files.includes(componentRequested)) {
+        const componentData = fs.readFileSync(`${componentsLocation}${componentRequested}`)
+        ctx.status = 200
+        ctx.body  = componentData
+    } else {
+        ctx.status = 404
     }
 }
 
