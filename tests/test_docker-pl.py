6c7b5e62c098897fc594c0879f75537dbd3d693d
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-04-02 01:37:54 -0400

    update PL image - reduced size by 10%; updated test-pl

--- tests/test_docker-pl.py
+++ tests/test_docker-pl.py
@@ -11,19 +11,14 @@ class TestLanguages(unittest.TestCase):
     def test_python(self):
         print("[INFO] Checking Python3 installation")
         response = client.containers.run("pl", command="which python3", remove=True, name="plcontainer").decode("utf-8")
-        self.assertEqual(response, "/usr/bin/python3\n", "Python3 missing!")
+        self.assertEqual(response, "/usr/local/bin/python3\n", "Python3 missing!")
     
     def test_cpp(self):
         print("[INFO] Checking gcc and gdb installation")
         response = client.containers.run("pl", command="which gcc", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/gcc\n", "gcc missing!")
-        response = client.containers.run("pl", command="which gdb", remove=True, name="plcontainer").decode("utf-8")
-        self.assertEqual(response, "/usr/bin/gdb\n", "gdb missing!")
-
-    def test_lisp(self):
-        print("[INFO] Checking Lisp installation")
-        response = client.containers.run("pl", command="which sbcl", remove=True, name="plcontainer").decode("utf-8")
-        self.assertEqual(response, "/usr/bin/sbcl\n", "Lisp (sbcl) missing!")
+        response = client.containers.run("pl", command="which g++", remove=True, name="plcontainer").decode("utf-8")
+        self.assertEqual(response, "/usr/bin/g++\n", "g++ missing!")
 
     def test_haskell(self):
         print("[INFO] Checking Haskell installation")
@@ -33,18 +28,23 @@ class TestLanguages(unittest.TestCase):
     def test_scheme(self):
         print("[INFO] Checking MIT-Scheme installation")
         response = client.containers.run("pl", command="which scheme", remove=True, name="plcontainer").decode("utf-8")
-        self.assertEqual(response, "/usr/bin/scheme\n", "MIT-Scheme missing!")
+        self.assertEqual(response, "/usr/local/bin/scheme\n", "MIT-Scheme missing!")
 
 class TestEditors(unittest.TestCase):
     def test_nano(self):
         print("[INFO] Checking Nano installation")
         response = client.containers.run("pl", command="which nano", remove=True, name="plcontainer").decode("utf-8")
-        self.assertEqual(response, "/bin/nano\n", "Nano missing!")
+        self.assertEqual(response, "/usr/bin/nano\n", "Nano missing!")
 
     def test_vim(self):
         print("[INFO] Checking vim installation")
         response = client.containers.run("pl", command="which vim", remove=True, name="plcontainer").decode("utf-8")
         self.assertEqual(response, "/usr/bin/vim\n", "vim missing!")
+    
+    def test_emacs(self):
+        print("[INFO] Checking Emacs installation")
+        response = client.containers.run("pl", command="which emacs", remove=True, name="plcontainer").decode("utf-8")
+        self.assertEqual(response, "/usr/bin/emacs\n", "Emacs missing!")
 
 class TestSampleCode(unittest.TestCase):
     def test_sample(self):
