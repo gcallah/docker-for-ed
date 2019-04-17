6125cb28bd82cfceb0b8f7b1650135a00bd4ee21
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-04-17 19:19:08 -0400

    fixed broken "makefile" download file link for cpp and pl images on home page

--- docker_for_edu_site/build/service-worker.js
+++ docker_for_edu_site/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/docker-for-ed/precache-manifest.8ee050740930e1c0b12971e44a490d94.js"
+  "/docker-for-ed/precache-manifest.5d7fc8b1718bb873b33b94972a33dbf4.js"
 );
 
 workbox.clientsClaim();
