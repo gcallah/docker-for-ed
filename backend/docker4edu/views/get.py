e25e89bdce20a183aab63c911116bdfd61f0f451
PrafulB <prtsh32@gmail.com>
2019-04-28 17:52:53 -0700

    Get component endpoint working

--- backend/docker4edu/views/get.py
+++ backend/docker4edu/views/get.py
@@ -1,16 +1,35 @@
-from django.http import HttpResponse, JsonResponse
+import os, json
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 
-def get(request):
+def component(request):
     allowedMethods = ['get', 'options']
-    if request.OPTIONS:
-        response = HttpResponse()
-        response['allow'] = ','.join([allowedMethods])
-        return response
-    
+    # if request.OPTIONS:
+    #     response = HttpResponse()
+    #     response['allow'] = ','.join([allowedMethods])
+    #     return response
+    print(request.GET)
     if request.GET:
         component = request.GET.get('component', 'all')
+        component = component.lower()
         componentData = retrieveComponent(component)
         return componentData
 
-def retrieveComponent(component):
+def retrieveComponent(requestedComponent):
+    print(requestedComponent)
+    componentsLocation = "{}/docker4edu/data".format(os.getcwd())
+    components = [componentFile.split('.json')[0] for componentFile in os.listdir(componentsLocation) ]
+    
+    if requestedComponent == "all":
+        return JsonResponse({"result": components})
+    
+    elif requestedComponent in components:
+        
+        with open("{}/{}.json".format(componentsLocation, requestedComponent)) as data:
+            component = json.load(data)
+        
+        return JsonResponse({"result": component})
+
+    else:
+        return HttpResponseNotFound('Component Not Found')
+
     return dict()
